86ea50d94135162321a295b1148dc4b169055f4a
Rafael Avila <rafa.avim@gmail.com>
2021-04-20 22:49:19 -0500

    Implements queue data structure.
    Adds property-based tests for queue data structure.

---
+++ Module4/Queue.cs
@@ -0,0 +1,70 @@
+﻿namespace aspnetapp.Module4
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Module3;
+
+    public class Queue<T> : IEnumerable<T>
+    {
+        public DoublyLinkedNode<T> Head { get; private set; }
+
+        public DoublyLinkedNode<T> Tail  { get; private set; }
+
+        public int Count { get; private set; }
+
+        public void QueueUp(T t)
+        {
+            var newNode = new DoublyLinkedNode<T>(t);
+            if (this.Count == 0)
+            {
+                this.Head = newNode;
+                this.Tail = this.Head;
+            }
+            else
+            {
+                newNode.Prev = this.Tail;
+                this.Tail.Next = newNode;
+                this.Tail = newNode;
+                
+            }
+            this.Count++;
+        }
+
+        public DoublyLinkedNode<T> Dequeue()
+        {
+            DoublyLinkedNode<T> dequeued = null;
+            if (this.Count > 0)
+            {
+                dequeued = this.Head;
+                this.Head = dequeued.Next;
+
+                if (this.Head == null) this.Tail = null;
+                if (this.Head != null) this.Head.Prev = null;
+
+                this.Count--;
+
+                dequeued.Next = null;
+                dequeued.Prev = null;
+                return dequeued;
+            }
+
+
+            return dequeued;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            var tmp = this.Head;
+            while (true)
+            {
+                yield return tmp.Value;
+                tmp = tmp.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var i in this) yield return i;
+        }
+    }
+}
---
+++ Module4/QueueTest.cs
@@ -0,0 +1,56 @@
+﻿namespace aspnetapp.Module4
+{
+    using System;
+    using FsCheck;
+    using FsCheck.NUnit;
+    using System.Linq;
+
+    [NUnit.Framework.TestFixture]
+    public class QueueTest
+    {
+        [Property]
+        public Property Size_Should_Increase_With_Every_New_Element_In_The_Queue(int[] src)
+        {
+            var queue = new Queue<int>();
+            for (int i = 0; i < src.Length; i++)
+            {
+                queue.QueueUp(src[i]);
+            }
+            return (queue.Count == src.Length).ToProperty();
+        }
+
+        [Property]
+        public Property Dequeueing_Should_Remove_First_Queued_Element(int[] src)
+        {
+            Func<bool> test = () => {
+                var firstInSrc = src[0];
+                var queue = new Queue<int>();
+                for (int i = 0; i < src.Length; i++)
+                {
+                    queue.QueueUp(src[i]);
+                }
+                var dequeued = queue.Dequeue();
+                return dequeued.Value == firstInSrc && queue.Count == src.Length - 1;
+            };
+            return test.When(src.Length > 0);
+        }
+
+        [Property]
+        public Property QueueingUp_Should_Always_Add_New_Tail(int[] src)
+        {
+            Func<bool> test = () => {
+                var firstInSrc = src[0];
+                var queue = new Queue<int>();
+
+                var alwaysAddedToTail = src.Aggregate<int, bool>(true, (acc, i) =>
+                {
+                    queue.QueueUp(i);
+                    return queue.Tail.Value == i;
+                });
+
+                return alwaysAddedToTail && queue.Count == src.Length;
+            };
+            return test.When(src.Length > 0);
+        }
+    }
+}
