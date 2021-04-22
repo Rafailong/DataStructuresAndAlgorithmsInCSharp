8191aeb68196ea5237f8dc0233716d20ef509ffd
Rafael Avila <rafa.avim@gmail.com>
2021-04-21 23:56:48 -0500

    Implements Deque.
    Stack and Queue combined in one data-strcuture.

---
+++ Module4/Deque.cs
@@ -0,0 +1,95 @@
+﻿namespace Module4
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Module3;
+
+    public class Deque<T> : IEnumerable<T>
+    {
+        public DoublyLinkedNode<T> Head { get; private set; }
+
+        public DoublyLinkedNode<T> Tail { get; private set; }
+
+        public int Count { get; private set; }
+
+        public void EnqueueHead(T t)
+        {
+            var newHead = new DoublyLinkedNode<T>(t);
+            var oldHead = this.Head;
+
+            this.Head = newHead;
+            this.Head.Next = oldHead;
+
+            if (this.Count == 0)
+            {
+                this.Tail = this.Head;
+            }
+            else
+            {
+                oldHead.Prev = this.Head;
+            }
+
+            this.Count++;
+        }
+
+        public void EnqueueTail(T t)
+        {
+            if (this.Count == 0) //  when list empty defer to AddHead
+            {
+                this.EnqueueHead(t);
+            }
+            else
+            {
+                var newTail = new DoublyLinkedNode<T>(t)
+                {
+                    Prev = this.Tail
+                };
+                this.Tail.Next = newTail; ;
+                this.Tail = newTail;
+                this.Count++;
+            }
+        }
+
+        public T DequeHead()
+        {
+            if (this.Count > 0)
+            {
+                var oldHead = this.Head;
+                this.Head = oldHead.Next;
+                this.Count--;
+                return oldHead.Value;
+            }
+
+            throw new InvalidOperationException("Imposible to DequeHead from an empty Deque");
+        }
+
+        public T DequeTail()
+        {
+            if (this.Count > 0)
+            {
+                var oldTail = this.Tail;
+                this.Tail = oldTail.Prev;
+                this.Count--;
+                return oldTail.Value;
+            }
+
+            throw new InvalidOperationException("Imposible to DequeTail from an empty Deque");
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var tmp = this.Head;
+            while (tmp != null)
+            {
+                yield return tmp.Value;
+                tmp = tmp.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Module4/DequeTest.cs
@@ -0,0 +1,103 @@
+﻿namespace Module4
+{
+    using System;
+    using FsCheck;
+    using FsCheck.NUnit;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    [NUnit.Framework.TestFixture]
+    public class DequeTest
+    {
+        [Property]
+        public Property Should_Expose_Stack_Push_Like_Operation(int[] input)
+        {
+            var dq = new Deque<int>();
+
+            Func<bool> property = () => {
+                var acc = input.Aggregate<int, bool>(true, (acc, i) =>
+                {
+                    dq.EnqueueHead(i);
+                    return dq.Head.Value == i;
+                });
+
+                Console.WriteLine(string.Join(',', dq.ToArray()));
+                Console.WriteLine($"Head={dq.Head.Value} - input.Last={input.Last()}");
+
+                return acc && dq.Count == input.Length;
+            };
+
+            return property.When(input.Length > 0);
+        }
+
+        [Property]
+        public Property Should_Expose_Stack_Pop_Like_Operation(List<int> input)
+        {
+            var dq = new Deque<int>();
+
+            Func<bool> property = () => {
+
+                // fill-in dq
+                input.ForEach(i => dq.EnqueueHead(i));
+                var filledUpSucceeded = dq.Count == input.Count;
+
+                // checking last-in-input head-in-dq
+                var acc = input.Reverse<int>().Aggregate(true, (acc, i) =>
+                {
+                    return dq.DequeHead() == i;
+                });
+
+                return filledUpSucceeded && acc && dq.Count == 0;
+            };
+
+            return property.When(input.Count > 0);
+        }
+
+        [Property]
+        public Property Should_Expose_Queue_QueueUp_Like_Operation(int[] input)
+        {
+            var dq = new Deque<int>();
+
+            Func<bool> property = () => {
+                var acc = input.Aggregate<int, bool>(true, (acc, i) =>
+                {
+                    dq.EnqueueTail(i);
+                    return dq.Tail.Value == i;
+                });
+
+                Console.WriteLine(string.Join(',', dq.ToArray()));
+                Console.WriteLine($"Tail={dq.Head.Value} - input.First={input.First()}");
+
+                return acc && dq.Count == input.Length;
+            };
+
+            return property.When(input.Length > 0);
+        }
+
+
+        [Property]
+        public Property Should_Expose_Queue_Dequeue_Like_Operation(List<int> input)
+        {
+            var dq = new Deque<int>();
+
+            Func<bool> property = () => {
+
+                // fill-in dq
+                // adding to head so first-in-input will be tail-in-dq
+                input.ForEach(i => dq.EnqueueHead(i));
+                var filledUpSucceeded = dq.Count == input.Count;
+
+                // checking first-in-input should be tail-in-dq
+                var acc = input.Aggregate(true, (acc, i) =>
+                {
+                    return dq.DequeTail() == i;
+                });
+
+                return filledUpSucceeded && acc && dq.Count == 0;
+            };
+
+            return property.When(input.Count > 0);
+        }
+
+    }
+}
