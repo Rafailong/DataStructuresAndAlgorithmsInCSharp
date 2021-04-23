b228c79333e9f6535a8872819dbad2c76ffb0a74
Rafael Avila <rafa.avim@gmail.com>
2021-04-23 00:28:10 -0500

    WIP - Binary Search Tree implementation.
    Adding lesser values to left (always) implemented and test.

---
+++ Module5/BinarySearchTree.cs
@@ -0,0 +1,58 @@
+﻿namespace Module5
+{
+    using System;
+
+    /**
+     * A binary tree where nodes with lesser values are placed to the left
+     * of the root, and nodes with equal or greater values are placed to the right.
+     */
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public BinaryTreeNode<T> Root { get; private set; }
+
+        public int Count { get; private set; }
+
+        public void Add(T t)
+        {
+            var newNode = new BinaryTreeNode<T>(t);
+
+            if (this.Count == 0)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                var tmp = this.Root;
+                do
+                {
+                    if (t.CompareTo(tmp.Value) < 0)
+                    {
+                        if (tmp.Left == null)
+                        {
+                            tmp.Left = newNode;
+                            break;
+                        }
+                        else
+                        {
+                            tmp = tmp.Left;
+                        }
+                    }
+                    else if(t.CompareTo(tmp.Value) >= 0)
+                    {
+                        if (tmp.Right == null)
+                        {
+                            tmp.Right = newNode;
+                            break;
+                        }
+                        else
+                        {
+                            tmp = tmp.Right;
+                        }
+                    }
+                } while (tmp != null);
+            }
+
+            this.Count++;
+        }
+    }
+}
---
+++ Module5/BinarySearchTreeTest.cs
@@ -0,0 +1,33 @@
+﻿namespace Module5
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BinarySearchTreeTest
+    {
+
+        [Test]
+        public void Always_Adds_Lesser_Values_To_The_Left()
+        {
+            var tree = new BinarySearchTree<int>();
+            var src = Enumerable.Range(0, 100).Reverse();
+            Console.WriteLine($"src=[{src.First()}..{src.Last()}]");
+
+            src.ToList().ForEach(i => tree.Add(i));
+
+            Assert.AreEqual(src.Count(), tree.Count);
+
+            var tmp = tree.Root;
+            var acc = true;
+            while (tmp != null)
+            {
+                acc = acc && tmp.Right == null;
+                tmp = tmp.Left;
+            }
+
+            Assert.IsTrue(acc);
+        }
+    }
+}
