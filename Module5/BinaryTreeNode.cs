c9fb6880198344562f76899da51f3c019c495f92
Rafael Avila <rafa.avim@gmail.com>
2021-04-23 00:27:21 -0500

    Adds generic binary tree node class.

---
+++ Module5/BinaryTreeNode.cs
@@ -0,0 +1,23 @@
+﻿namespace Module5
+{
+    public class BinaryTreeNode<T>
+    {
+        public T Value { get; private set; }
+
+        public BinaryTreeNode<T> Left { get; set; }
+
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T v)
+        {
+            this.Value = v;
+        }
+
+        public BinaryTreeNode(T v, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            this.Value = v;
+            this.Left = left;
+            this.Right = right;
+        }
+    }
+}
